d118f08319ba99eeabcc4e4b16630c131dc91faa
Yeshwanth Pandeti <yeshwanthpandeti@ENCOPDBANLT1876.local>
2022-09-14 02:00:00 +0530

    Contains linux practise files

---
+++ Shell/functions.sh
@@ -0,0 +1,10 @@
+!#/bin/sh
+
+number_one() {
+	echo "Roger one"
+	number_two
+}
+number_two() {
+	echo "reger 2"
+}
+number_one
---
+++ Shell/shellargs.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "File Name : $0"
+echo "First Parameter : $1"
+echo "Second Paramet : $2"
+echo "Quoted Values : $@"
+echo "Quoted Values : $*"
+echo "No of Parameters : $#"
